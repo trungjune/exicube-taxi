c9e74e69fd112ca4e1d408b5ac80da311abf4246
trungth <trungth@vnpt-technology.vn>
2023-07-24 16:12:15 +0700

    Second commit

---
+++ mobile-app/config/AppConfig.js
@@ -0,0 +1,12 @@
+module.exports.AppConfig = {
+    app_name: 'Your App Name', 
+    app_description: 'Your App Description Here', 
+    app_identifier: 'com.something.app', 
+    ios_app_version: '1.0.0', 
+    android_app_version: 100, 
+    facebookAppId: '12345',
+    facebookClientToken: 'XXXXXXXXXXXXXXX',
+    expo_owner: 'XXXXXXXXXXXXXXX',
+    expo_slug: 'XXXXXXXXXXXXXXX',
+    expo_project_id: 'XXXXXXXXXXXXXXX'
+}
